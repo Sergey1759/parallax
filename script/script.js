e9d1e48e19e7d913a82a68f828f288ed71f65dd0
Sergey1759 <lisichka1759@gmail.com>
2019-05-20 15:12:11 +0300

    add Layer

--- script/script.js
+++ script/script.js
@@ -1,111 +1,118 @@
+function Layer (id){
+    this.elem = document.querySelector(id);
+    this.top  = parseInt(getComputedStyle(this.elem).top);
+    this.left = parseInt(getComputedStyle(this.elem).left);
+    this.setLeft = function setLeft(left){
+        this.elem.style.left = left+'px';
+    }
+    this.setTop = function setTop(top){
+        this.elem.style.top = top+'px';
+    }
+}
+
+const ClientWidth  = document.documentElement.clientWidth;
+const ClientHeight = document.documentElement.clientHeight;
+
+const layer_background = new Layer('#layer-background');;
+const layer_1          = new Layer('#layer-1');
+const layer_2          = new Layer('#layer-2');
+const layer_3          = new Layer('#layer-3');
+const layer_4          = new Layer('#layer-4');
+const layer_5          = new Layer('#layer-5');
+const layer_6          = new Layer('#layer-6');
+const layer_7          = new Layer('#layer-7');
+const layer_8          = new Layer('#layer-8');
+const layer_9          = new Layer('#layer-9');
+
+if (window.innerWidth < 768){         
+layer_background.elem.style.width  = `${ ClientWidth  }px`;
+layer_background.elem.style.height = `${ ClientHeight }px`;
+
+
+function Axis(obj,px,position,axis,BeginMovement,EndMovement){   
+    let rate = ((position * 100)/EndMovement);
+    console.log(rate);
+    let start = (axis == 'X') ? obj.left : obj.top;
+    let rate_position = px*(rate/100);
+    if(position > BeginMovement && position < EndMovement){
+        if (axis == 'X') { obj.setLeft(start - rate_position) } else {obj.setTop(start - rate_position)}
+    } else if(position < BeginMovement && position > -EndMovement){
+        rate_position *= -1;
+        if (axis == 'X') { obj.setLeft(start + rate_position) } else {obj.setTop(start +  rate_position)}
+    } 
+}
+    
+window.addEventListener('deviceorientation', function(event) {
+    Axis(layer_1,60,event.gamma,'X',0,60);
+    Axis(layer_1,40,event.beta,'Y',0,60);
+    
+    Axis(layer_2,20,event.gamma,'X',0,60);
+    Axis(layer_2,10,event.beta,'Y',0,60);
+    
+    Axis(layer_3,270,event.gamma,'X',0,60);
+    Axis(layer_3,170,event.beta,'Y',0,60);
+    
+    Axis(layer_4,300,event.gamma,'X',0,60);
+    Axis(layer_4,50,event.beta,'Y',0,60);
+    
+    Axis(layer_5,10,event.gamma,'X',0,60);
+    Axis(layer_5,10,event.beta,'Y',0,60);
+    
+    Axis(layer_6,180,event.gamma,'X',0,60);
+    Axis(layer_6,180,event.beta,'Y',0,60);
+    
+    });
+}
 if(window.innerWidth > 768){
-const layer_background = document.querySelector('#layer-background');
-const layer_1 = document.querySelector('#layer-1');
-const layer_2 = document.querySelector('#layer-2');
-const layer_3 = document.querySelector('#layer-3');
-const layer_4 = document.querySelector('#layer-4');
-const layer_5 = document.querySelector('#layer-5');
-const layer_6 = document.querySelector('#layer-6');
-const layer_7 = document.querySelector('#layer-7');
-const layer_8 = document.querySelector('#layer-8');
-const layer_9 = document.querySelector('#layer-9');
+layer_background.elem.style.width  = `${ ClientWidth * 3 }px`;
+layer_background.elem.style.height = `${ ClientHeight * 3}px`;
 
 
-let ClientWidth  = document.documentElement.clientWidth;
-let ClientHeight = document.documentElement.clientHeight;
-
-layer_background.style.width  = `${ ClientWidth * 3 }px`;
-layer_background.style.height = `${ ClientHeight * 3}px`;
-
-
-
-let arr = [[],[],[],[],[],[],[],[],[],[]];
-arr[0][0] = layer_1;
-arr[0][1] =  parseInt(getComputedStyle(layer_1).left);
-arr[0][2] =  parseInt(getComputedStyle(layer_1).top);
-
-arr[1][0] = layer_2;
-arr[1][1] =  parseInt(getComputedStyle(layer_2).left);
-arr[1][2] =  parseInt(getComputedStyle(layer_2).top);
-
-arr[2][0] = layer_3;
-arr[2][1] =  parseInt(getComputedStyle(layer_3).left);
-arr[2][2] =  parseInt(getComputedStyle(layer_3).top);
-
-arr[3][0] = layer_4;
-arr[3][1] =  parseInt(getComputedStyle(layer_4).left);
-arr[3][2] =  parseInt(getComputedStyle(layer_4).top);
-
-arr[4][0] = layer_background;
-arr[4][1] =  parseInt(getComputedStyle(layer_background).left);
-arr[4][2] =  parseInt(getComputedStyle(layer_background).top);
-
-arr[5][0] = layer_5;
-arr[5][1] =  parseInt(getComputedStyle(layer_5).left);
-arr[5][2] =  parseInt(getComputedStyle(layer_5).top);
-
-arr[6][0] = layer_6;
-arr[6][1] =  parseInt(getComputedStyle(layer_6).left);
-arr[6][2] =  parseInt(getComputedStyle(layer_6).top);
-
-arr[7][0] = layer_7;
-arr[7][1] =  parseInt(getComputedStyle(layer_7).left);
-arr[7][2] =  parseInt(getComputedStyle(layer_7).top);
-
-arr[8][0] = layer_8;
-arr[8][1] =  parseInt(getComputedStyle(layer_8).left);
-arr[8][2] =  parseInt(getComputedStyle(layer_8).top);
-
-arr[9][0] = layer_9;
-arr[9][1] =  parseInt(getComputedStyle(layer_9).left);
-arr[9][2] =  parseInt(getComputedStyle(layer_9).top);
-
-function axisX(arr,px,position){
+function axisX(obj,px,position){
     let ratePosition = ClientWidth/100/2;
     if(position>(ClientWidth/2) && position != 0){
         position = position - ClientWidth/2;
         let rate = ((position * 100)/(ClientWidth/2));
-        arr[0].style.left = (arr[1]-(px*(rate/100)))+'px';
+        obj.setLeft(obj.left-(px*(rate/100)));
     } else if(position<(ClientWidth/2) && position != 0){
         let rate = 100 - ((position * 100)/(ClientWidth/2));
-        arr[0].style.left = (arr[1]+(px*(rate/100))) + 'px';
+        obj.setLeft(obj.left+(px*(rate/100)));
     } 
 }
-
-function axisY(arr,px,position){
+function axisY(obj,px,position){
     let ratePosition = ClientHeight/100/2;
     if(position>(ClientHeight/2) && position != 0){
         position = position - ClientHeight/2;
         let rate = ((position * 100)/(ClientHeight/2));
-        arr[0].style.top = (arr[2]-(px*(rate/100)))+'px';
+        obj.setTop(obj.top-(px*(rate/100)));
     } else if(position<(ClientHeight/2) && position != 0){
         let rate = 100 - ((position * 100)/(ClientHeight/2));
-        arr[0].style.top = (arr[2]+(px*(rate/100))) + 'px';
+        obj.setTop(obj.top+(px*(rate/100)));
     } 
 }
 
 function mouse(event){
-     axisX(arr[4],700,event.clientX);
-     axisX(arr[0],500,event.clientX);
-     axisX(arr[1],500,event.clientX);
-     axisX(arr[3],100,event.clientX);
-     axisX(arr[2],500,event.clientX);
-     axisX(arr[5],ClientWidth,event.clientX);
-     axisX(arr[6],200,event.clientX);
-     axisX(arr[7],500,event.clientX);
-     axisX(arr[8],500,event.clientX);
-     axisX(arr[9],500,event.clientX);
+     axisX(layer_background,700,event.clientX);
+     axisX(layer_1,500,event.clientX); 
+     axisX(layer_2,500,event.clientX); 
+     axisX(layer_3,500,event.clientX); 
+     axisX(layer_4,100,event.clientX); 
+     axisX(layer_5,ClientWidth,event.clientX); 
+     axisX(layer_6,200,event.clientX); 
+     axisX(layer_7,500,event.clientX); 
+     axisX(layer_8,500,event.clientX); 
+     axisX(layer_9,500,event.clientX); 
      
-     axisY(arr[4],500,event.clientY);
-     axisY(arr[0],300,event.clientY);
-     axisY(arr[1],500,event.clientY);
-     axisY(arr[3],100,event.clientY);
-     axisY(arr[2],500,event.clientY);
-     axisY(arr[5],ClientHeight,event.clientY);
-     axisY(arr[6],200,event.clientY);
-     axisY(arr[7],500,event.clientY);
-     axisY(arr[8],500,event.clientY);
-     axisY(arr[9],500,event.clientY);
+     axisY(layer_background,500,event.clientY);
+     axisY(layer_1,300,event.clientY);
+     axisY(layer_2,500,event.clientY);
+     axisY(layer_3,500,event.clientY);
+     axisY(layer_4,100,event.clientY);
+     axisY(layer_5,ClientHeight,event.clientY);
+     axisY(layer_6,200,event.clientY);
+     axisY(layer_7,500,event.clientY);
+     axisY(layer_8,500,event.clientY);
+     axisY(layer_9,500,event.clientY);
 }
 }
 
